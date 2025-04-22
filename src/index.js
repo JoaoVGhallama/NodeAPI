9be19abdfcbcd804159e24753439329a07f5b4bc
JoaoVGhallama <joao.vghallama@gmail.com>
2025-04-21 23:15:10 -0300

    add get requisition for all users & by id

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ let usuarios = [
   { id: 2, nome: "Maria" },
 ];
 
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
+});
+
+app.get("/usuarios/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const usuario = usuarios.find((u) => u.id === id);
+  if (!usuario)
+    return res.status(404).json({ mensagem: "Usuário não encontrado" });
+  res.json(usuario);
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
