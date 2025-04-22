a3f580cd2fb44885efec2a533433fc2b88cbc80c
JoaoVGhallama <joao.vghallama@gmail.com>
2025-04-21 23:14:34 -0300

    add mock data for users

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const PORT = 3000;
 
 app.use(express.json());
 
+let usuarios = [
+  { id: 1, nome: "João" },
+  { id: 2, nome: "Maria" },
+];
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
